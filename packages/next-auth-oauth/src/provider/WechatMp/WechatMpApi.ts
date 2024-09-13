946aa0aabbd9efb223d130078ea9a403afe11fc6
liuhuapiaoyuan <278780765@qq.com>
2024-09-13 19:53:47 +0800

    fix: 自动处理加密数据情况

--- packages/next-auth-oauth/src/provider/WechatMp/WechatMpApi.ts
+++ packages/next-auth-oauth/src/provider/WechatMp/WechatMpApi.ts
@@ -1,5 +1,15 @@
 import crypto from "crypto";
+export type COMMON_WECHATMP_MESSAGE = {
+  
+  FromUserName: string;
+  ToUserName: string;
+  MsgType: string;
+  Content: string;
+  MsgId: string;
+  EventKey?: string;
+  Encrypt?:string
 
+}
 type AccessTokenApiResult = {
   access_token: string;
   /**
@@ -92,10 +102,18 @@ export class WechatMpApi {
   private appId: string;
   private appSecret: string;
   private accessTokenCacheManager: WehcatMpAccessTokenCacheManager;
-
+  /* 解密密钥*/
+  private encoderAESKey?: string;
+  /* 消息令牌 */
+  private token?: string;
   constructor(options?: {
     appId: string;
     appSecret: string;
+    /* 解密密钥*/
+    encoderAESKey?: string;
+    /* 消息令牌 */
+    token?: string;
+    /* Token缓存管理器 */
     accessTokenCacheManager: WehcatMpAccessTokenCacheManager;
   }) {
     this.appId = options?.appId || process.env.AUTH_WECHATMP_APP_ID!;
@@ -104,6 +122,9 @@ export class WechatMpApi {
     this.accessTokenCacheManager =
       options?.accessTokenCacheManager ||
       new InMemoryWechatMpAccessTokenCacheManager();
+    this.token = options?.token ?? process.env.AUTH_WECHATMP_TOKEN;
+    this.encoderAESKey =
+      options?.encoderAESKey ?? process.env.AUTH_WECHATMP_ENCODER_AESKEY;
   }
 
   private async getAccessTokenFromBackend() {
@@ -165,4 +186,95 @@ export class WechatMpApi {
   getPermanentQrcodeUrl(ticket: string) {
     return `https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=${ticket}`;
   }
+
+  /**
+   * 验证签名
+   * 按照 加密方式,如果encrypt_type是aes，会通过msg_signature自动验签
+   * 如果不是aes，则会通过 signature 字段验签
+   * @param query 
+   */
+  checkSignature(searchParams:URLSearchParams , Encrypt?:string){
+    let signature = searchParams.get("signature");
+    const timestamp = searchParams.get("timestamp");
+    const nonce = searchParams.get("nonce");
+    const encrypt_type = searchParams.get("encrypt_type");
+
+    //timestamp  nonce 必须有内容，用字符串判断
+    if (!timestamp || !nonce) {
+      throw new Error('签名验证失败:timestamp 或 nonce 为空');
+    }
+
+    let tmpArr = [this.token, timestamp, nonce];
+    if(encrypt_type && encrypt_type =='aes'){
+      if(typeof Encrypt== 'undefined' || Encrypt===''){
+        throw new Error('加密消息缺失:{Encrypt}');
+      }
+      tmpArr = [this.token, timestamp, nonce , Encrypt];
+      signature = searchParams.get("msg_signature");
+    }
+    tmpArr.sort();
+    const tmpStr = tmpArr.join("");
+    const hash = crypto.createHash("sha1").update(tmpStr).digest("hex");
+    return hash === signature;
+  }
+
+   decryptMessage(Encrypt: string) {
+    // 1. Base64 解码 EncodingAESKey，生成 32 字节的 AESKey
+    const AESKey = Buffer.from(this.encoderAESKey + "=", "base64");
+    // 2. Base64 解码 Encrypt 密文，得到 TmpMsg
+    const encryptedMessage = Buffer.from(Encrypt, "base64");
+    // 3. 使用 AESKey 对 TmpMsg 进行 AES 解密（CBC 模式）
+    const decipher = crypto.createDecipheriv(
+      "aes-256-cbc",
+      AESKey,
+      AESKey.subarray(0, 16)
+    ); // IV 是前 16 字节的 AESKey
+    decipher.setAutoPadding(false); // 手动处理填充
+
+    let decrypted = Buffer.concat([
+      decipher.update(encryptedMessage),
+      decipher.final(),
+    ]);
+    decrypted = unpadPKCS7(decrypted);
+    return decrypted.toString();
+  }
+  /**
+   *  加密消息
+   * @param xml xml格式的消息
+   */
+  encryptMessage(xml: string) {
+
+     // 1. 生成 AESKey
+     const AESKey = Buffer.from(this.encoderAESKey + "=", "base64");
+
+     // 2. 生成 FullStr
+     const random = crypto.randomBytes(16); // 16 字节随机字符串
+     const msg = Buffer.from(xml, 'utf-8'); // 明文消息
+     const msg_len = Buffer.alloc(4);
+     msg_len.writeUInt32BE(msg.length, 0); // 网络字节序
+     // 拼接 FullStr
+     const FullStr = Buffer.concat([random, msg_len, msg, Buffer.from(this.appId, 'utf-8')]);
+     // 3. PKCS7 填充
+     const blockSize = 32; // AES 密钥长度
+     const paddedFullStr = padPKCS7(FullStr, blockSize);
+     // 4. 使用 AESKey 加密
+     const cipher = crypto.createCipheriv('aes-256-cbc', AESKey, AESKey.subarray(0, 16)); // IV 为 AESKey 的前 16 字节
+     let encrypted = Buffer.concat([
+         cipher.update(paddedFullStr),
+         cipher.final()
+     ]);
+     // 5. Base64 编码
+     return encrypted.toString('base64');
+  }
+}
+// 辅助函数：PKCS7 解码
+function unpadPKCS7(buffer: Buffer) {
+  const pad = buffer[buffer.length - 1];
+  return buffer.slice(0, -pad);
+}
+// 辅助函数：PKCS7 填充
+function padPKCS7(buffer:Buffer, blockSize:number) {
+  const pad = blockSize - (buffer.length % blockSize);
+  const padding = Buffer.alloc(pad, pad);
+  return Buffer.concat([buffer, padding]);
 }
--- packages/next-auth-oauth/src/provider/WechatMp/index.ts
+++ packages/next-auth-oauth/src/provider/WechatMp/index.ts
@@ -5,16 +5,18 @@ import {
   UserinfoEndpointHandler,
 } from "next-auth/providers";
 import { NextRequest } from "next/server";
-import { checkSignature, parseWehcatMessageXML, WechatMpApi } from "./WechatMpApi";
-import { WechatMpCaptchaManager } from "./WechatPlatformConfig";
+import { COMMON_WECHATMP_MESSAGE, parseWehcatMessageXML, WechatMpApi } from "./WechatMpApi";
+import { WechatMpCaptchaManager } from "./WechatMpCaptchaManagerConfig";
 
+const globalForPrisma = globalThis as unknown as {
+  wechatMpCaptchaManager: WechatMpCaptchaManager;
+};
 
-const globalForPrisma = globalThis as unknown as {wechatMpCaptchaManager:WechatMpCaptchaManager}
- 
-export const wechatMpCaptchaManager = globalForPrisma.wechatMpCaptchaManager || new WechatMpCaptchaManager()
- 
-if (process.env.NODE_ENV !== "production") globalForPrisma.wechatMpCaptchaManager = wechatMpCaptchaManager
+export const wechatMpCaptchaManager =
+  globalForPrisma.wechatMpCaptchaManager || new WechatMpCaptchaManager();
 
+if (process.env.NODE_ENV !== "production")
+  globalForPrisma.wechatMpCaptchaManager = wechatMpCaptchaManager;
 
 export type WechatPlatformConfig = {
   clientId?: string;
@@ -29,12 +31,20 @@ export type WechatPlatformConfig = {
    * QRCODE 临时二维码
    * @default "MESSAGE"
    */
-  type:"MESSAGE"|"QRCODE"
+  type: "MESSAGE" | "QRCODE";
   /**
    * 认证账号必须提供
    * 提供二维码创建工具，
    */
-  wechatMpApi?:WechatMpApi
+  wechatMpApi?: WechatMpApi;
+  /**
+   * 扫码页面地址
+   */
+  qrcodePage?: string;
+  /**
+   * 接收微信消息的webhook地址
+   */
+  wechatMessageWebhook?: string;
 };
 
 export type WechatMpProfile = {
@@ -45,78 +55,71 @@ export type WechatMpProfile = {
   unionid: string;
 };
 
-
 /**
  * 处理Nextjs的GET/POST请求
  */
-async function handler(req: NextRequest, token: string) {
+async function handler(req: NextRequest, wechatMpApi: WechatMpApi) {
   const searchParams = req.nextUrl.searchParams;
-  const signature = searchParams.get("signature");
   const echostr = searchParams.get("echostr");
-  const timestamp = searchParams.get("timestamp");
-  const nonce = searchParams.get("nonce");
-  if (req.method == "GET" ) {
-    if (
-      signature &&
-      echostr &&
-      timestamp &&
-      nonce &&
-      checkSignature(signature, timestamp, nonce, token)
-    ) {
+  const encrypt_type = searchParams.get("encrypt_type");
+
+  if (req.method == "GET") {
+    if (wechatMpApi.checkSignature(searchParams)) {
       return new Response(echostr, { status: 200 });
     }
     return new Response("Invalid signature", { status: 401 });
   }
   // 判断消息类型 如果是表单类型
-  const isFormData = req.headers.get("Content-Type")?.includes("form")
+  const isFormData = req.headers.get("Content-Type")?.includes("form");
   if (isFormData) {
     const formData = await req.formData();
     const code = formData.get("code");
     const data = await wechatMpCaptchaManager.data(code?.toString()!);
-    if(!data){
+    if (!data) {
       return new Response("Invalid state", { status: 401 });
     }
     return Response.json({
-      scope:"openid",
+      scope: "openid",
       access_token: data.openid,
       token_type: "bearer",
     });
   } else {
-    if (
-      !(
-        signature &&
-        timestamp &&
-        nonce &&
-        checkSignature(signature, timestamp, nonce, token)
-      )
-    ) {
-      return new Response("Invalid signature", { status: 401 });
-    }
     const xml = await req.text();
     // 文本消息
-    const message = parseWehcatMessageXML<{
-      FromUserName: string;
-      ToUserName: string;
-      MsgType: string;
-      Content: string;
-      MsgId: string;
-      EventKey?:string
-    }>(xml);
+    let message = parseWehcatMessageXML<COMMON_WECHATMP_MESSAGE>(xml);
+    if (!wechatMpApi.checkSignature(searchParams , message.Encrypt)) {
+      return new Response("Invalid signature", { status: 401 });
+    }
+    const isEncrypt = message.Encrypt && message.Encrypt !== '';
+    if(isEncrypt && message.Encrypt){
+      message = parseWehcatMessageXML<COMMON_WECHATMP_MESSAGE>(
+        wechatMpApi.decryptMessage(message.Encrypt)
+      );
+    }
     // 如果有 Ticket 优先使用 Ticket 验证
-    const code =  message.EventKey ?? message.Content
-    const status = await wechatMpCaptchaManager.complted(code,{
-      openid: message.FromUserName
-    })
-    return new Response(
-      `<xml>
+    const code = message.EventKey ?? message.Content;
+    const status = await wechatMpCaptchaManager.complted(code, {
+      openid: message.FromUserName,
+    });
+    let content = `<xml>
   <ToUserName><![CDATA[${message.FromUserName}]]></ToUserName>
   <FromUserName><![CDATA[${message.ToUserName}]]></FromUserName>
-  <CreateTime>${Math.floor(Date.now()/1000)}</CreateTime>
+  <CreateTime>${Math.floor(Date.now() / 1000)}</CreateTime>
   <MsgType><![CDATA[text]]></MsgType>
-  <Content><![CDATA[${status?"登录成功":"登录失败,请重新获得验证码"}]]></Content>
-</xml>
-`,
-      { status: 200, headers:{"Content-Type": "text/xml"} }
+  <Content><![CDATA[${
+    status ? "登录成功" : "登录失败,请重新获得验证码"
+  }]]></Content>
+</xml>`
+    if (isEncrypt) {
+      content = wechatMpApi.encryptMessage(content);
+      content = `<xml>
+  <Encrypt><![CDATA[${content}]]></Encrypt>
+  <ToUserName><![CDATA[${message.FromUserName}]]></ToUserName>
+</xml>`
+    }
+
+    return new Response(content,
+      { status: 200, headers: { "Content-Type": "text/xml" } }
     );
   }
 }
@@ -130,17 +133,18 @@ async function handler(req: NextRequest, token: string) {
  */
 export default function WeChatMp<P extends WechatMpProfile>(
   options?: OAuthUserConfig<P> & WechatPlatformConfig
-): OAuth2Config<P> & { 
-  handler:{
-    GET: (req:NextRequest)=>Promise<Response>,
-    POST: (req:NextRequest)=>Promise<Response> , 
-  }
-  getScanUrl:()=>Promise<{
-    qrcode:string,
-    type:"MESSAGE"|"QRCODE",
-    ticket:string
-  }>
-  options?: OAuthUserConfig<P> & WechatPlatformConfig } {
+): OAuth2Config<P> & {
+  handler: {
+    GET: (req: NextRequest) => Promise<Response>;
+    POST: (req: NextRequest) => Promise<Response>;
+  };
+  getScanUrl: () => Promise<{
+    qrcode: string;
+    type: "MESSAGE" | "QRCODE";
+    ticket: string;
+  }>;
+  options?: OAuthUserConfig<P> & WechatPlatformConfig;
+} {
   const {
     clientId = process.env.AUTH_WECHATMP_APP_ID!,
     clientSecret = process.env.AUTH_WECHATMP_APP_SECRET!,
@@ -148,22 +152,25 @@ export default function WeChatMp<P extends WechatMpProfile>(
     token = process.env.AUTH_WECHATMP_TOKEN!,
     type = process.env.AUTH_WECHATMP_TYPE ?? "MESSAGE",
     wechatMpApi,
+    qrcodePage = "http://localhost:3000/auth/qrcode",
+    wechatMessageWebhook = "http://localhost:3000/api/auth/wechatmp",
     // 令牌 开发者ID，开发者密码，消息加解密密钥
   } = options ?? {};
-  if (!clientId || !clientSecret || !token ) {
-    throw new Error("WechatMp platform requires client_id, client_secret, token");
+  if (!clientId || !clientSecret || !token) {
+    throw new Error(
+      "WechatMp platform requires client_id, client_secret, token"
+    );
   }
-  if(type === "QRCODE" && typeof wechatMpApi === 'undefined'){
-    throw new Error("WechatMpApi is required for QRCODE type")
+  if (type === "QRCODE" && typeof wechatMpApi === "undefined") {
+    throw new Error("WechatMpApi is required for QRCODE type");
   }
-   
 
   const authorization: AuthorizationEndpointHandler = {
-    url: "http://localhost:3000/auth/qrcode",
+    url: qrcodePage,
     params: {
       appid: clientId,
       response_type: "code",
-      state:"wechatmp"
+      state: "wechatmp",
     },
   };
 
@@ -190,36 +197,40 @@ export default function WeChatMp<P extends WechatMpProfile>(
     id: "wechatmp",
     name: "微信公众号关注",
     type: "oauth",
-    style: { logo: "/providers/wechatOfficialAccount.svg", bg: "#fff", text: "#000" },
+    style: {
+      logo: "/providers/wechatOfficialAccount.svg",
+      bg: "#fff",
+      text: "#000",
+    },
     checks: ["none"],
     clientId,
     clientSecret,
     authorization,
     token: {
-      url: "http://localhost:3000/api/auth/wechatmp",
+      url: wechatMessageWebhook,
     },
     userinfo,
     profile,
-    handler:{
-      GET:(req:NextRequest)=>handler(req,token),
-      POST:(req:NextRequest)=>handler(req,token)  
+    handler: {
+      GET: (req: NextRequest) => handler(req, token),
+      POST: (req: NextRequest) => handler(req, token),
     },
-    async getScanUrl(){
-      const code = await wechatMpCaptchaManager.generate()
-      if(type === "QRCODE" && wechatMpApi){
-        const {  url } = await wechatMpApi.createPermanentQrcode(code)
+    async getScanUrl() {
+      const code = await wechatMpCaptchaManager.generate();
+      if (type === "QRCODE" && wechatMpApi) {
+        const { url } = await wechatMpApi.createPermanentQrcode(code);
         return {
           qrcode: url,
-          ticket:code,
-          type : "QRCODE"
-        }
-      }else{
+          ticket: code,
+          type: "QRCODE",
+        };
+      } else {
         return {
           qrcode: "https://cdn.kedao.ggss.club/picgo0",
           ticket: code,
-          type : "MESSAGE"
-        }
+          type: "MESSAGE",
+        };
       }
-    }
+    },
   };
 }
