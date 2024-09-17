550f44bdfbe16e07bf2fb450524a1ab158e99f1f
liuhuapiaoyuan <278780765@qq.com>
2024-09-18 00:25:38 +0800

    fix: theme switcher error

--- packages/example/src/app/(app)/_components/ThemeSwitcher.tsx
+++ packages/example/src/app/(app)/_components/ThemeSwitcher.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import {
     DropdownMenu,
     DropdownMenuContent,
+    DropdownMenuItem,
     DropdownMenuLabel,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
@@ -13,23 +14,25 @@ export function ThemeSwitcher() {
             <Button
                 variant="outline"
                 size="icon"
-                className="overflow-hidden rounded-full"
+                className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground size-8"
             >
-                <Sun />
+                <div>
+                <Sun width={15}/>
+                </div>
             </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
-            <DropdownMenuLabel >
+            <DropdownMenuItem >
                 <div className="flex  items-center gap-1">
-                    <Sun />白天
+                    <Sun width={15} />白天
                 </div>
-            </DropdownMenuLabel>
-            <DropdownMenuLabel>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
                 <div className="flex items-center gap-1">
-                    <Moon />
+                    <Moon width={15}/>
                     夜间
                 </div>
-            </DropdownMenuLabel>
+            </DropdownMenuItem>
         </DropdownMenuContent>
     </DropdownMenu>
 }
