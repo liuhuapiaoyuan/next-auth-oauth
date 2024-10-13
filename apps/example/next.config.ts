fa9e3239de53aba675053bd1e0dcd7c53fa76fc6
liuhuapiaoyuan <278780765@qq.com>
2024-10-13 11:12:46 +0800

    fix: remove config

--- apps/example/next.config.ts
+++ apps/example/next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  transpilePackages: ['next-auth-oauth'],
-}
+const nextConfig: NextConfig = {}
 
 export default nextConfig
