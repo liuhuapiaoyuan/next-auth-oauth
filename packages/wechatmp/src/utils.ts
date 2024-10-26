50151b9a22fb0a6fe847af1c2cd9e84b32e586e5
liuhuapiaoyuan <278780765@qq.com>
2024-10-26 10:34:54 +0800

    feat: add tests for parseWehcatMessageXML and renderXML functions

--- packages/wechatmp/src/utils.ts
+++ packages/wechatmp/src/utils.ts
@@ -4,7 +4,7 @@
  * @returns
  */
 export function parseWehcatMessageXML<T>(xml: string) {
-  const result: Record<string, string> = {}
+  const result: Record<string, unknown> = {}
   // 匹配 XML 标签及其内容
   const regex = /<(\w+)>([\s\S]*?)<\/\1>/g
   let match
@@ -15,6 +15,9 @@ export function parseWehcatMessageXML<T>(xml: string) {
     // 尝试嵌套
     if (tag === 'xml') {
       return parseWehcatMessageXML(cleanContent)
+    } else if (cleanContent.includes('<')) {
+      const nestedResult = parseWehcatMessageXML(cleanContent)
+      result[tag] = nestedResult
     } else {
       result[tag] = cleanContent
     }
@@ -23,21 +26,51 @@ export function parseWehcatMessageXML<T>(xml: string) {
   return result as T
 }
 
+function _renderXML(data: Record<string, string | number | object>): string {
+  const xmls = []
+  for (const key in data) {
+    const value = data[key]
+    if (typeof value === 'number') {
+      xmls.push(`<${key}>${value}</${key}>`)
+    } else if (typeof value == 'object') {
+      xmls.push(`<${key}>
+  ${_renderXML(value as Record<string, string | number | object>)}
+</${key}>`)
+    } else {
+      xmls.push(`<${key}><![CDATA[${value}]]></${key}>`)
+    }
+  }
+  return xmls.join('')
+}
 /**
  * 提供 XML 输出
  * @param data
  * @returns
  */
-export function renderXML(data: Record<string, string | number>) {
+export function renderXML(data: Record<string, string | number | object>) {
   const xmls = ['<xml>']
-  for (const key in data) {
-    const value = data[key]
-    if (typeof value === 'number') {
-      xmls.push(`<${key}>${value}</${key}>`)
-    } else {
-      xmls.push(`<${key}><![CDATA[${value}]]></${key}>`)
-    }
-  }
+  xmls.push(_renderXML(data))
   xmls.push('</xml>')
   return xmls.join('')
 }
+const t1 = parseWehcatMessageXML<
+  Record<string, string | number | object>
+>(`<xml>
+  <ToUserName><![CDATA[toUser]]></ToUserName>
+  <FromUserName><![CDATA[fromUser]]></FromUserName>
+  <CreateTime>
+    <A>1348831860</A>
+    <B>1348831860</B>
+  </CreateTime>
+  <MsgType><![CDATA[text]]></MsgType>
+  <Content><![CDATA[this is a test]]></Content>
+  <MsgId>1234567890123456</MsgId>
+  <MsgDataId>xxxx</MsgDataId>
+  <Idx>xxxx</Idx>
+  </xml>
+  `)
+console.log({ t1 })
+const t2 = renderXML(t1)
+console.log({ t2 })
+const t3 = parseWehcatMessageXML(t2)
+console.log({ t3 })
